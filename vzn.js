f7792dfe250d6be23339eebda2775ef604288cdc
VznBot <victomiguel2013@gmail.com>
2026-03-24 21:42:02 -0300

    Update vzn.js

--- vzn.js
+++ vzn.js
@@ -1,4 +1,16 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot online");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
 
 const client = new Client({
   intents: [
@@ -8,7 +20,9 @@ const client = new Client({
   ]
 });
 
-client.login(process.env.TOKEN);
+client.on("ready", () => {
+  console.log(`Logado como ${client.user.tag}`);
+});
 
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
